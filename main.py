ca232aa141c77b05a45f97fcc7e6f8b1e1fb2aa5
melliott93 <melliott93@gmail.com>
2022-12-24 10:50:38 +0100

    Fix some stuff to work without telegram

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def striphtml(data):
     return n.sub(' ',p.sub('', data))
 
 last_add = datetime.now()
-date = today.strftime("%m/%d/%y %H:%M")
+date = last_add.strftime("%m/%d/%y %H:%M")
 
-telegram_client = TelegramClient('name', telegram_api_id, telegram_api_hash)
+# telegram_client = TelegramClient('name', telegram_api_id, telegram_api_hash)
 
 url = f"https://www.olx.pl/d/nieruchomosci/stancje-pokoje/warszawa/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=600&search%5Bfilter_float_price:to%5D=1100"
 while True:
@@ -31,24 +31,26 @@ while True:
             added = bs.find("span", class_='css-19yf5ek')
             added = re.match(r'''^.*\">(.*)\</.*$''',str(added))
                 
-                title = bs.find("h1", class_='css-swd4zc-TextStyled er34gjf0')
-                title = re.match(r'''^.*\">(.*)\</.*$''',str(title))
-                description = striphtml(str(bs.find("div", class_="css-12l22jb-TextStyled er34gjf0")))
-                price = bs.find("h3", class_="css-19cr6mc-TextStyled er34gjf0")
-                price = re.match(r'''^.*\">(.*)\</.*$''',str(price))
-                price = ''.join(e for e in str(price.group(1)) if e.isalnum())
-                images = bs.find_all("img", class_="css-1bmvjcs")
+            title = bs.find("h1", class_='css-swd4zc-TextStyled er34gjf0')
+            title = re.match(r'''^.*\">(.*)\</.*$''',str(title))
+            description = striphtml(str(bs.find("div", class_="css-12l22jb-TextStyled er34gjf0")))
+            price = bs.find("h3", class_="css-19cr6mc-TextStyled er34gjf0")
+            price = re.match(r'''^.*\">(.*)\</.*$''',str(price))
+            price = ''.join(e for e in str(price.group(1)) if e.isalnum())
+            images = bs.find_all("img", class_="css-1bmvjcs")
 
-                final = []
-                final.append(added.group(1))
-                final.append(title.group(1))
-                final.append(description)
-                final.append(price)
-                images = []
-                for image in images:
-                    try:
-                        final.append(image["src"])
-                    except:
-                        final.append(image["data-src"])
-                telegram_client.send_message(chat, final, file=images)
+            final = []
+            final.append(added.group(1))
+            final.append(title.group(1))
+            final.append(description)
+            final.append(price)
+            images1 = []
+            for image in images:
+                try:
+                    images1.append(image["src"])
+                except:
+                    images1.append(image["data-src"])
+            print(final)
+            print(images1)
+            # telegram_client.send_message(chat, final, file=images)
     time.sleep(300)
